b45bb1b9adcba591e0353f5cc9bacb410e700179
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-09-01 08:45:22 +0300

    dockstrings

--- tgbot/services/game.py
+++ tgbot/services/game.py
@@ -11,10 +11,18 @@ from tgbot.services.printer import print_dice, print_emotion
 
 
 async def roll_dice(num: int = 5) -> list[int]:
+    """
+    Функция бросает кубики в количестве num (по умолчанию 5).
+    :param num: int: количество кубиков для броска.
+    :return: list[int]: список с целыми числами.
+    """
     return [randint(1, 6) for _ in range(num)]
 
 
 async def save_result(mark: int, summa: int, dice_list: list, save_for: str, state: FSMContext) -> None:
+    """
+    Функция перезаписывает состояния игроков после каждого броска.
+    """
     async with state.proxy() as data:
         if save_for == 'player':
             data['player_mark'] = mark
@@ -27,6 +35,12 @@ async def save_result(mark: int, summa: int, dice_list: list, save_for: str, sta
 
 
 async def check_combination(dice_set: list[int]) -> tuple[int, int, str]:
+    """
+    Функция проверяет комбинацию игрока. Ставит оценку mark,
+    подсчитывает сумму summa (нужно для определения победителя при совпадении оценок) и определяет название комбинации.
+    :param dice_set: список с целыми числами - комбинация кубиков
+    :return: кортеж: (mark: оценка, summa: сумма, result: название выпавшей комбинации)
+    """
     combination_dict = Counter(dice_set)  # {5: 4, 6: 1}
     combination_dict_values = sorted(list(combination_dict.values()), reverse=True)  # [4, 1]
 
@@ -70,6 +84,11 @@ async def check_combination(dice_set: list[int]) -> tuple[int, int, str]:
 
 
 async def play_turn(message: Message) -> tuple[int, int, str, list]:
+    """
+    Управляющая функция. Вызывает функцию броска кубиков roll_dice.
+    Вызывает функцию проверки комбинации check_combination. Вызывает функцию печати кубиков print_dice.
+    :return: кортеж: (mark: оценка, summa: сумма, result: название выпавшей комбинации, dice_list: список кубиков)
+    """
     await sleep(2)
     dice_list = await roll_dice()
     mark, summa, result = await check_combination(dice_list)
@@ -78,6 +97,15 @@ async def play_turn(message: Message) -> tuple[int, int, str, list]:
 
 
 async def play_reroll(message: Message, state: FSMContext, play_for: str) -> tuple[int, int, str, list]:
+    """
+    Управляющая функция переброса кубиков.
+    В зависимости от того, кто осуществляет переброс (play_for == 'player'|'bot') получает из состояния его кубики,
+    которые игрок пожелал не перебрасывать.
+    Определяет количество кубиков для переброса num.
+    Вызывает функцию броска кубиков roll_dice.
+    Вызывает функцию проверки комбинации check_combination. Вызывает функцию печати кубиков print_dice.
+    :return: кортеж: (mark: оценка, summa: сумма, result: название выпавшей комбинации, dice_list: список кубиков)
+    """
     await sleep(2)
     states = await state.get_data()
     if play_for == 'player':
@@ -92,7 +120,11 @@ async def play_reroll(message: Message, state: FSMContext, play_for: str) -> tup
     return mark, summa, result, result_dice_list
 
 
-async def ask_reroll(message: Message, state: FSMContext):
+async def ask_reroll(message: Message, state: FSMContext) -> None:
+    """
+    Функция предлагает игроку перебросить кубики.
+    Показывает инлайн-клавиатуру players_reroll с выбором кубиков для переброса.
+    """
     states = await state.get_data()
     player_dice_list = states.get('player_dice_list')
     await message.answer('Выбирай, какие кубики желаешь перебросить и жми "Готово!"\n'
@@ -103,9 +135,6 @@ async def ask_reroll(message: Message, state: FSMContext):
 async def should_bot_reroll(message: Message, state: FSMContext) -> bool:
     """
     Функция определяет, должен ли бот перебрасывать кубики.
-    :param message: aiogram.types.Message
-    :param state: aiogram.dispatcher.FSMContext
-    :return: bool
     """
     states = await state.get_data()
     last_winner = states.get('last_winner')
@@ -134,9 +163,7 @@ async def choose_dices_for_bots_reroll(message: Message, state: FSMContext) -> s
     Функция, определяющая, какие кубики бот должен сохранить, а какие перебросить.
     Перезаписывает состояние 'bot_dice_list' - кубики, которые не будут переброшены.
     Возвращает строку.
-    :param message: aiogram.types.Message
-    :param state: aiogram.dispatcher.FSMContext
-    :return: str - 'all' - если бот решил перебросить все кубики; 'some' - если бот выбрал несколько кубиков для переброса
+    :return: str: 'all' - если бот решил перебросить все кубики; 'some' - если бот выбрал несколько кубиков для переброса
     """
     states = await state.get_data()
     bot_mark = states.get('bot_mark')
@@ -174,6 +201,9 @@ async def choose_dices_for_bots_reroll(message: Message, state: FSMContext) -> s
 
 
 async def reward_player(state: FSMContext) -> None:
+    """
+    Функция добавляет 1 очко к счету игрока и перезаписывает состояния player_score и last_winner.
+    """
     states = await state.get_data()
     score = states.get('player_score')
     score += 1
@@ -183,6 +213,9 @@ async def reward_player(state: FSMContext) -> None:
 
 
 async def reward_bot(state: FSMContext) -> None:
+    """
+    Функция добавляет 1 очко к счету бота и перезаписывает состояния bot_score и last_winner.
+    """
     states = await state.get_data()
     score = states.get('bot_score')
     score += 1
@@ -192,6 +225,9 @@ async def reward_bot(state: FSMContext) -> None:
 
 
 async def finish_game(message: Message, player_score: int, bot_score: int, state: FSMContext) -> None:
+    """
+    Функция показывает итоговый результат игры и в зависимости от него - эмоциональную реакцию бота.
+    """
     await state.finish()
     await message.answer(f"🏁 Игра окончена со счётом:\n"
                          f"🤵 Ты <b>{player_score}:{bot_score}</b> Бот 👤", reply_markup=main_actions)
@@ -202,6 +238,10 @@ async def finish_game(message: Message, player_score: int, bot_score: int, state
 
 
 async def set_winner(message: Message, state: FSMContext) -> None:
+    """
+    Функция определяет победителя раунда. Вызывает функцию reward_player или reward_bot для записи результата
+    и показывает сообщение с клавиатурой do_next (следующий раунд).
+    """
     states = await state.get_data()
     player_mark = states.get('player_mark')
     player_summa = states.get('player_summa')
@@ -226,7 +266,11 @@ async def set_winner(message: Message, state: FSMContext) -> None:
     await message.answer(text, reply_markup=await do_next())
 
 
-async def play_round(message: Message, state: FSMContext):
+async def play_round(message: Message, state: FSMContext) -> None:
+    """
+    Функция начинает новый раунд. Показывает сообщение с текущим счетом.
+    В зависимости от того, кто победил в прошлом раунде - тому предлагает сделать бросок нажатием на инлайн-кнопку.
+    """
     states = await state.get_data()
     round_counter = states.get('round_counter')
     last_winner = states.get('last_winner')
